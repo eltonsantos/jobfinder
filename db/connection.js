703b7fa2e265f46bf6a960ce191e7cf0d9f2ae4a
Elton Santos <eltonaxl@hotmail.com>
2022-08-16 01:28:52 -0300

    Trying connect postgres on heroku

--- db/connection.js
+++ db/connection.js
@@ -12,6 +12,24 @@ const Sequelize = require('sequelize');
 /* HEROKU
 postgres://qtfzszxdvkcxrh:d4af303e2005512cd239e2cfeb7ebbb5afb4a870a5077802b3e272986398688c@ec2-52-86-56-90.compute-1.amazonaws.com:5432/d730earqbnv78
 */
-const sequelize = new Sequelize('postgres://qtfzszxdvkcxrh:d4af303e2005512cd239e2cfeb7ebbb5afb4a870a5077802b3e272986398688c@ec2-52-86-56-90.compute-1.amazonaws.com:5432/d730earqbnv78');
+
+// DATABASE_URL='postgres://qtfzszxdvkcxrh:d4af303e2005512cd239e2cfeb7ebbb5afb4a870a5077802b3e272986398688c@ec2-52-86-56-90.compute-1.amazonaws.com:5432/d730earqbnv78'
+
+//const sequelize = new Sequelize(DATABASE_URL);
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+  dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
+   },
+});
 
 module.exports = sequelize
