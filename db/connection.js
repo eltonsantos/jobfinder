7f54629932d7e859154b73fdc7edb089c33788c2
Elton Santos <eltonaxl@hotmail.com>
2022-08-16 00:46:37 -0300

    Change config to postgres in heroku

--- db/connection.js
+++ db/connection.js
@@ -7,6 +7,11 @@ const Sequelize = require('sequelize');
 // });
 
 // POSTGRESQL
-const sequelize = new Sequelize('postgres://postgres@localhost:5432/jobsfinder', {dialect: 'postgres'}) // Example for postgres
+//const sequelize = new Sequelize('postgres://postgres@localhost:5432/jobsfinder', {dialect: 'postgres'}) // Example for postgres
+
+/* HEROKU
+postgres://qtfzszxdvkcxrh:d4af303e2005512cd239e2cfeb7ebbb5afb4a870a5077802b3e272986398688c@ec2-52-86-56-90.compute-1.amazonaws.com:5432/d730earqbnv78
+*/
+const sequelize = new sequelize('postgres://qtfzszxdvkcxrh:d4af303e2005512cd239e2cfeb7ebbb5afb4a870a5077802b3e272986398688c@ec2-52-86-56-90.compute-1.amazonaws.com:5432/d730earqbnv78');
 
 module.exports = sequelize
